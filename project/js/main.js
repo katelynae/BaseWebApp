2da76666e08c2277faa0aebc0f6fcd7fc9e85f42
keklund <keklund@hubspot.com>
2019-10-27 14:54:31 +0000

    tech test 4

--- project/js/main.js
+++ project/js/main.js
@@ -7,6 +7,6 @@ function getHubDB() {
 	var url = 'https://api.hubapi.com/hubdb/api/v2/tables/1036302?portalId=2832391';
 
 	$.ajax(url,{success: function(data){
-		console.log(data.name);
+		console.log("Name: "+data.name);
 	}})
 }
